4ff5f388d2809166f4787f4727d71752a1efe024
artmetkachenko <artemaprela@gmail.com>
2025-12-24 01:20:57 +0200

    Update Task2.java

--- Task8/Task2.java
+++ Task8/Task2.java
@@ -9,7 +9,6 @@ public class Task2 {
         String str = sc.nextLine();
         String strRev = "";
 
-        // Цикл проходить по рядку з кінця до початку і додає кожен символ у зворотному порядку
         for(int i = str.length() - 1; i >= 0; i--){
             strRev += str.charAt(i);
         }
