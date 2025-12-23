95ddccadb8c3afd5ed78600e0e914ae7f23c73be
artmetkachenko <artemaprela@gmail.com>
2025-12-24 01:21:20 +0200

    Update Task5.java

--- Task8/Task5.java
+++ Task8/Task5.java
@@ -1,7 +1,6 @@
 package org.college.practice_work_8;
 
 import java.util.Scanner;
-// Мама сказала що дасть мені мандарини якщо я закрию сесію та отримаю стипендію
 public class Task5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
